038327356c99659310b2bdb9fafe4c771c4618b4
slogvo <86196042+slogvo@users.noreply.github.com>
2025-02-18 13:40:26 +0700

    remove unused favicon and delete obsolete Vue components and API code

--- pages/learn/vue-base.vue
+++
@@ -1,9 +0,0 @@
-import { VBaseDefineModel } from '../../.nuxt/components';
-<script setup></script>
-
-<template>
-  <div>
-    <h1>Learn Vue Concepts</h1>
-    <VBaseDefineModel />
-  </div>
-</template>
